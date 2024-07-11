f4617cdb1211bbfc4a06e0eaaa63a6a77069c5ce
Pravallika <pravallikajampana2003@gmail.com>
2024-07-11 15:03:48 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export const data = [
+  {
+    question:
+      "Which type of malware is designed to replicate itself and spread to other computers?",
+    option1: "Virus",
+    option2: "Worm",
+    option3: "Trojan",
+    option4: "Spyware",
+    ans: 2,
+  },
+  {
+    question: "What is the primary function of a firewall in a network?",
+
+    option1: "To detect viruses",
+    option2: "To block unauthorized access",
+    option3: "To speed up the network",
+    option4: "To backup data",
+    ans: 2,
+  },
+  {
+    question:
+      "Which type of attack involves overwhelming a system with traffic to render it unusable?",
+
+    option1: "Phishing",
+    option2: "Man-in-the-middle",
+    option3: "DDoS",
+    option4: "SQL Injection",
+    ans: 3,
+  },
+  {
+    question: "What does the term 'phishing' refer to in cybersecurity?",
+    option1: "A method to secure data transmissions",
+    option2: "An attack to trick users into revealing personal information",
+    option3: "A technique to test network security",
+    option4: "A type of firewall",
+    ans: 2,
+  },
+  {
+    question:
+      "Which encryption method uses two keys, one for encryption and another for decryption?",
+    option1: "Symmetric Encryption",
+    option2: "Asymmetric Encryption",
+    option3: "Hashing",
+    option4: "Steganography",
+    ans: 2,
+  },
+];
